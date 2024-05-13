facd3868abd8f096a1ad04ff6a972a54852a7b39
Mazay85 <55110519+Mazay85@users.noreply.github.com>
2024-05-13 23:23:38 +0300

    Update cports.sh

--- cports.sh
+++ cports.sh
@@ -2,14 +2,14 @@ PORT=335
 if ss -tulpen | awk '{print $5}' | grep -q ":26656$" ; then
     echo -e "\e[31mPort 26656 already in use.\e[39m"
     sleep 2
-    sed -i -e "s|:26656\"|:${PORT}56\"|g" $DAEMON_HOME/config/config.toml
+    sed -i -e "s|:26656\"|:${PORT}56\"|g" .0gchain/config/config.toml
     echo -e "\n\e[42mPort 26656 changed to ${PORT}56.\e[0m\n"
     sleep 2
 fi
 if ss -tulpen | awk '{print $5}' | grep -q ":26657$" ; then
     echo -e "\e[31mPort 26657 already in use\e[39m"
     sleep 2
-    sed -i -e "s|:26657\"|:${PORT}57\"|" $DAEMON_HOME/config/config.toml
+    sed -i -e "s|:26657\"|:${PORT}57\"|" .0gchain/config/config.toml
     echo -e "\n\e[42mPort 26657 changed to ${PORT}57.\e[0m\n"
     sleep 2
     $DAEMON_NAME config node tcp://localhost:${PORT}57
@@ -17,49 +17,49 @@ fi
 if ss -tulpen | awk '{print $5}' | grep -q ":26658$" ; then
     echo -e "\e[31mPort 26658 already in use.\e[39m"
     sleep 2
-    sed -i -e "s|:26658\"|:${PORT}58\"|" $DAEMON_HOME/config/config.toml
+    sed -i -e "s|:26658\"|:${PORT}58\"|" .0gchain/config/config.toml
     echo -e "\n\e[42mPort 26658 changed to ${PORT}58.\e[0m\n"
     sleep 2
 fi
 if ss -tulpen | awk '{print $5}' | grep -q ":6060$" ; then
     echo -e "\e[31mPort 6060 already in use.\e[39m"
     sleep 2
-    sed -i -e "s|:6060\"|:${PORT}60\"|" $DAEMON_HOME/config/config.toml
+    sed -i -e "s|:6060\"|:${PORT}60\"|" .0gchain/config/config.toml
     echo -e "\n\e[42mPort 6060 changed to ${PORT}60.\e[0m\n"
     sleep 2
 fi
 if ss -tulpen | awk '{print $5}' | grep -q ":1317$" ; then
     echo -e "\e[31mPort 1317 already in use.\e[39m"
     sleep 2
-    sed -i -e "s|:1317\"|:${PORT}17\"|" $DAEMON_HOME/config/app.toml
+    sed -i -e "s|:1317\"|:${PORT}17\"|" .0gchain/config/app.toml
     echo -e "\n\e[42mPort 1317 changed to ${PORT}17.\e[0m\n"
     sleep 2
 fi
 if ss -tulpen | awk '{print $5}' | grep -q ":9090$" ; then
     echo -e "\e[31mPort 9090 already in use.\e[39m"
     sleep 2
-    sed -i -e "s|:9090\"|:${PORT}90\"|" $DAEMON_HOME/config/app.toml
+    sed -i -e "s|:9090\"|:${PORT}90\"|" .0gchain/config/app.toml
     echo -e "\n\e[42mPort 9090 changed to ${PORT}90.\e[0m\n"
     sleep 2
 fi
 if ss -tulpen | awk '{print $5}' | grep -q ":9091$" ; then
     echo -e "\e[31mPort 9091 already in use.\e[39m"
     sleep 2
-    sed -i -e "s|:9091\"|:${PORT}91\"|" $DAEMON_HOME/config/app.toml
+    sed -i -e "s|:9091\"|:${PORT}91\"|" .0gchain/config/app.toml
     echo -e "\n\e[42mPort 9091 changed to ${PORT}91.\e[0m\n"
     sleep 2
 fi
 if ss -tulpen | awk '{print $5}' | grep -q ":8545$" ; then
     echo -e "\e[31mPort 8545 already in use.\e[39m"
     sleep 2
-    sed -i -e "s|:8545\"|:${PORT}45\"|" $DAEMON_HOME/config/app.toml
+    sed -i -e "s|:8545\"|:${PORT}45\"|" .0gchain/config/app.toml
     echo -e "\n\e[42mPort 8545 changed to ${PORT}45.\e[0m\n"
     sleep 2
 fi
 if ss -tulpen | awk '{print $5}' | grep -q ":8546$" ; then
     echo -e "\e[31mPort 8546 already in use.\e[39m"
     sleep 2
-    sed -i -e "s|:8546\"|:${PORT}46\"|" $DAEMON_HOME/config/app.toml
+    sed -i -e "s|:8546\"|:${PORT}46\"|" .0gchain/config/app.toml
     echo -e "\n\e[42mPort 8546 changed to ${PORT}46.\e[0m\n"
     sleep 2
 fi
